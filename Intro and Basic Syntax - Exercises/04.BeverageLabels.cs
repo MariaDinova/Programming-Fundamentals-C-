6af801da4e34551d40e1d3a5e19940acfe6b52b9
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-07-06 09:57:53 +0300

    Add files via upload

---
+++ Intro and Basic Syntax - Exercises/04.BeverageLabels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            double energy100 = double.Parse(Console.ReadLine());
+            double sugar100 = double.Parse(Console.ReadLine());
+
+            double energy = volume * energy100 / 100;
+            double sugar = volume * sugar100 / 100;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energy}kcal, {sugar}g sugars");
+        }
+    }
+}
---
+++ Intro and Basic Syntax - Exercises/05.CharacterStats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.CharacterStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int curHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int curEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine("Health: |{0}{1}|", new string('|', curHealth), new string('.', maxHealth - curHealth));
+            Console.WriteLine("Energy: |{0}{1}|", new string('|', curEnergy), new string('.', maxEnergy - curEnergy));
+        }
+    }
+}
